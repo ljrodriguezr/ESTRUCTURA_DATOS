9746819e1f18a170a2bd35e70aca91eb0b66acfd
LUCIA RODRIGUEZ <lj.rodriguezr@uea.edu.ec>
2024-12-14 09:47:02 -0500

    area de rectangulo

--- Tarea2/Program.cs
+++ Tarea2/Program.cs
@@ -1,3 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Universidad Estatal Amazonica");
 Console.WriteLine();
+
+Rectangulo figura1 = new Rectangulo(2,3);
+Console.WriteLine("EL area del rectangulo es:" + figura1.Area());
