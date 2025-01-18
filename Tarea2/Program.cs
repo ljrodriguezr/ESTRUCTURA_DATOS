bc7eb80064117e81e1d2391eccf6dc6ce82aea3c
LUCIA RODRIGUEZ <lj.rodriguezr@uea.edu.ec>
2025-01-17 21:49:20 -0500

    pruebaejercicio3

--- Tarea2/Program.cs
+++ Tarea2/Program.cs
@@ -1,3 +1,4 @@
 ﻿Console.WriteLine("Universidad Estatal Amazonica");
 //ejercicio1.Run();
-ejercicio2.Run();
+//ejercicio2.Run();
+ejercicio3.Run();
--- Tarea2/obj/Debug/net9.0/Tarea2.AssemblyInfo.cs
+++ Tarea2/obj/Debug/net9.0/Tarea2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tarea2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fad743d2ddea19b34d0274ef45ae04ae98851d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40d110ae0850d4905bb51dc93510a0b8b6a6fad1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tarea2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tarea2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
